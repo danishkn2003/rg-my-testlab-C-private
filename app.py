cb67e8efc54fd079aab0a799a0c179b279577a6c
danishkn2003 <danishkn2003@outlook.com>
2025-11-22 21:28:33 +0530

    Update app and deployment files + add workflow

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from AKS + ACR + CI/CD!"
+    return "Hello from AKS + ACR + CI/CD! + Updated"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
